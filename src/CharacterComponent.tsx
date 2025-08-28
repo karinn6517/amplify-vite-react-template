bf0971f5f6e6169e468c0280359f288840b8bb08
karinn6517 <karinn6517@gmail.com>
2025-08-28 08:23:36 +0000

    各数値の実際の値を記載

--- src/CharacterComponent.tsx
+++ src/CharacterComponent.tsx
@@ -1,87 +1,95 @@
-import { Radio, Typography } from "@mui/material";
-import "./CharacterComponent.scss";
-import React from "react";
 import { Button } from "@aws-amplify/ui-react";
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { Typography } from "@mui/material";
+import React from "react";
+import "./CharacterComponent.scss";
 
-export enum CharacterState {
-  None,
-  Glow,
-  Halo,
-}
-
+//上（kabaneri がわ）から設定する数値
 export interface CharacterComponentProps {
   className: string;
-  tortal?: number;
-  chance?: number;
-  state?: CharacterState;
-  czUp?: boolean;
-  onChance: (isLow: boolean) => void;
-  onCZone: () => void;
+  allStar: number;
+  atUp: number;
+  onChance?: (glowStar: boolean) => void;
 }
-
 
 const CharacterComponent: React.FC<CharacterComponentProps> = (props) => {
   const [total, setTotal] = React.useState(0);
-  const [chance, setChance] = React.useState(0);
-  const [state, setState] = React.useState(CharacterState.None);
-  const [czUp, setCzUp] = React.useState(false);
+  const [star, setStar] = React.useState(false);
 
-  // badge ようにいれとく
-  //const [invisible, setInvisible] = React.useState(false);
-
+  // atUp にはいったらやるところ
   React.useEffect(() => {
-    setTotal(props.tortal ?? 0);
-  }, [props.tortal]);
+    setTotal(0);
+    setStar(false);
+  }, [props.atUp]);
 
+  // allStar にはいったらやるところ
+  const isFirstAllStar = React.useRef(true);
   React.useEffect(() => {
-    setChance(props.chance ?? 0);
-  }, [props.chance]);
-
-  React.useEffect(() => {
-    setState(props.state ?? CharacterState.None);
-  }, [props.state]);
-
-  React.useEffect(() => {
-    setCzUp(props.czUp ?? false);
-  }, [props.czUp]);
-
+    if (isFirstAllStar.current) {
+      isFirstAllStar.current = false;
+      return;
+    }
+    setTotal(total + 45);
+    setStar(true);
+  }, [props.allStar]);
 
   return (
     <div className={`${props.className} character-component`}>
-      <div className="box lamp">
-        <button></button>
-        <Radio
-          checked={state === CharacterState.None}
-          value={CharacterState.None}
-          name={`level-${props.className}`}
-          onChange={() => setState(CharacterState.None)}
-        />
-      </div>
-      <div className="box lamp">
-        <button className="glow"></button>
-        <Radio
-          checked={state === CharacterState.Glow}
-          value={CharacterState.Glow}
-          name={`level-${props.className}`}
-          onChange={() => setState(CharacterState.Glow)}
-        />
-      </div>
-      <div className="box lamp">
-        <button className="halo"></button>
-        <Radio
-          checked={state === CharacterState.Halo}
-          value={CharacterState.Halo}
-          name={`level-${props.className}`}
-          onChange={() => setState(CharacterState.Halo)}
-        />
+      <div>
+        <Typography variant="h5" align="right"><span>{total}</span> pt</Typography>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+          <div className="box lamp">
+            <button onClick={() => {
+              setTotal(total + 1);
+              // chanceevent
+              // 低確発光しないので false のまま
+              props.onChance && props.onChance(false);
+            }}></button>
+          </div>
+          <div className="box lamp">
+            <button className="glow" onClick={() => {
+              setTotal(total + 15);
+              // chanceevent
+              // false から true に変わるとき低確発光扱い
+              props.onChance && props.onChance(star === false);
+              setStar(true);
+            }}></button>
+          </div>
+          <div className="box lamp">
+            <button className="halo" onClick={() => {
+              setTotal(total + 15);
+              // chanceevent
+              // false から true に変わるとき低確発光扱い
+              props.onChance && props.onChance(star === false);
+              setStar(true);
+            }}></button>
+          </div>
+        </div>
       </div>
       <div className="box">
-        <Typography variant="h5" align="right">123pt</Typography>
-        <Button>CZ</Button>
-        <Button>-D</Button>
-        {total} / {chance} / {state} / {czUp}
+        <Button onClick={
+          () => {
+            // 無星に戻す
+            setStar(false)
+          }
+        }><StarBorderIcon color="action" /></Button>
+        <Button onClick={
+          () => {
+            // cz に入ったのでカウンタも星もリセット
+            setTotal(0);
+            setStar(false);
+          }
+        }><WhatshotIcon color="success" /></Button>
+        <Button onClick={
+          () => {
+            // マニュアルでの15pt追加
+            setTotal(total + 15);
+          }
+        }><ControlPointIcon color="secondary" /></Button>
       </div>
-    </div>
+    </div >
   );
 };
 
--- src/Kabaneri.tsx
+++ src/Kabaneri.tsx
@@ -3,37 +3,45 @@ import "./Kabaneri.css";
 import Button from '@mui/material/Button';
 import CharacterComponent from "./CharacterComponent";
 import { Stack, Typography } from "@mui/material";
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import AvTimerIcon from '@mui/icons-material/AvTimer';
+import React from "react";
 
 function Kabaneri() {
 
-  function chanceEvent(isLow: boolean): void {
-    console.log(isLow);
+  const [allStar, setAllStarCount] = React.useState(0);
+  const [atUp, setAtUpCount] = React.useState(0);
+  const [chanceCount, setChanceCount] = React.useState(0);
+  const [highChanceCount, setHighChanceCount] = React.useState(0);
+
+  function onChanceEvent(glowStar: boolean) {
+    setChanceCount(chanceCount + 1);
+    if (glowStar) {
+      setHighChanceCount(highChanceCount + 1);
+    }
   }
 
-  function czoneEvent(): void {
-
-  }
   return (
     <main className="kabaneri-page">
       <h1>Kabaneriページ</h1>
       <Stack direction="row" alignItems="center" justifyContent="space-between">
         <Stack direction="row" spacing={2} alignItems="center">
-          <Typography variant="h5">5</Typography>
+          <Typography variant="h5">{highChanceCount}</Typography>
           <Typography variant="h4">/</Typography>
-          <Typography variant="h5">10</Typography>
+          <Typography variant="h5">{chanceCount}</Typography>
         </Stack>
         <Stack direction="row" spacing={4}>
-          <Button variant="contained" color="primary" onClick={() => alert("Button clicked!")}>
-            Allstar
+          <Button variant="contained" color="primary" onClick={() => setAllStarCount(allStar + 1)}>
+            <AlternateEmailIcon />
           </Button>
-          <Button variant="contained" color="secondary" onClick={() => alert("Button clicked!")}>
-            AT
+          <Button variant="contained" color="success" onClick={() => setAtUpCount(atUp + 1)}>
+            <AvTimerIcon />
           </Button>
         </Stack>
       </Stack>
-      <CharacterComponent className={"mumei"} tortal={100} onChance={chanceEvent} onCZone={czoneEvent}></CharacterComponent>
-      <CharacterComponent className={"ikoma"} tortal={100} onChance={chanceEvent} onCZone={czoneEvent}></CharacterComponent>
-      <CharacterComponent className={"biba"} tortal={100} onChance={chanceEvent} onCZone={czoneEvent}></CharacterComponent>
+      <CharacterComponent className={"mumei"} allStar={allStar} atUp={atUp} onChance={onChanceEvent}></CharacterComponent>
+      <CharacterComponent className={"ikoma"} allStar={allStar} atUp={atUp} onChance={onChanceEvent}></CharacterComponent>
+      <CharacterComponent className={"biba"} allStar={allStar} atUp={atUp} onChance={onChanceEvent}></CharacterComponent>
     </main >
   );
 }
